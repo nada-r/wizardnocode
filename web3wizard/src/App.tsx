e61d4670f7c2fa7c90a700580952160ba0687c1b
nada-r <nadarzer@gmail.com>
2023-09-15 01:10:20 +0200

    Fix route management

--- web3wizard/src/App.tsx
+++ web3wizard/src/App.tsx
@@ -1,17 +1,25 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
 import Step1 from './Step1';
 
 const App: React.FC = () => {
     return (
         <Router>
-            <Switch>
-                <Route exact path="/" render={() => <Redirect to="/step1" />} />
-                <Route path="/step1" component={Step1} />
-                {/* autres routes */}
-            </Switch>
+            <Routes>
+                <Route path="/" element={<RedirectToStep1 />} />
+                <Route path="/step1" element={<Step1 />} />
+            </Routes>
         </Router>
     );
+}
+
+const RedirectToStep1: React.FC = () => {
+    const navigate = useNavigate();
+    React.useEffect(() => {
+        navigate('/step1');
+    }, [navigate]);
+
+    return null;
 }
 
 export default App;
--- web3wizard/src/Step1.tsx
+++ web3wizard/src/Step1.tsx
@@ -10,12 +10,10 @@ const Step1: React.FC = () => {
                 <option value="scenario1">Scenario 1</option>
                 <option value="scenario2">Scenario 2</option>
                 <option value="scenario3">Scenario 3</option>
-                {/* Add as many scenarios as you have */}
             </select>
 
             <button onClick={() => {
                 if (selectedScenario) {
-                    // Navigate to the next step or save the selected scenario in the state
                 } else {
                     alert('Please select a scenario');
                 }
