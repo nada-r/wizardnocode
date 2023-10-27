8d9ba70fa128842349f6d9a628a90ed73fa4969b
nada-r <nadarzer@gmail.com>
2023-10-27 23:58:19 +0200

    add children layout component

--- web3wizard/src/App.tsx
+++ web3wizard/src/App.tsx
@@ -2,16 +2,33 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
 import Step1 from './Step1';
 
+const Layout: React.FC = ({ children }) => {
+    return (
+        <div className="layout">
+            {/* */}
+            <div className="content">{children}</div>
+            {/*  */}
+        </div>
+    );
+};
+
+
 const App: React.FC = () => {
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<RedirectToStep1 />} />
-                <Route path="/step1" element={<Step1 />} />
+                <Route path="/step1" element={
+                    <Layout>
+                        <Step1 />
+                    </Layout>
+                } />
+                {/* Ajoutez d'autres étapes de la même manière */}
             </Routes>
         </Router>
     );
 }
+
 
 const RedirectToStep1: React.FC = () => {
     const navigate = useNavigate();
